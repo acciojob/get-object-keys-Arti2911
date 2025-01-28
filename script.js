ca42eaf2f4c14ad944bc8a7820947768ccf1f136
Arti2911 <139011882+Arti2911@users.noreply.github.com>
2025-01-28 22:12:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const student = {
+  name: "John Doe"
+};
+
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+const keys = getKeys(student);
+console.log(keys);
+
+console.log(keys.includes("name"));
